80a42733a202c3be3a08bb1dab5b74b553645720
zaurmag411@gmail.com <guKGyVmC7o>
2022-07-16 00:32:38 +0300

    Upd: Script task
    Add copy path

--- gulp/js.js
+++ gulp/js.js
@@ -1,9 +1,12 @@
+let path = require('./path/path.js')
+
 module.exports = function () {
 	return $.gulp.task('js:build', (done) => {
 		$.gulp.src($.webpackConfig.entry)
 			.pipe($.plugins.plumber())
 			.pipe($.webpackStream($.webpackConfig))
 			.pipe($.gulp.dest($.webpackConfig.output.path))
+			.pipe($.gulp.dest(path.path.copyPath + '/js/'))
 			.pipe($.browserSync.reload({ stream: true }))
 		done()
 	})
